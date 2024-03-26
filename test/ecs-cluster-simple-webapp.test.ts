745a7f92d579c64ae5559c6a69510df8e4239bdf
James Trenholme <james.trenholme@nimbleapproach.com>
2024-03-26 14:50:17 +0000

    Initial commit

---
+++ test/ecs-cluster-simple-webapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsClusterSimpleWebapp from '../lib/ecs-cluster-simple-webapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-cluster-simple-webapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsClusterSimpleWebapp.EcsClusterSimpleWebappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
